bd81cee89843f7fe0cb9b2cfc1598bf634197665
schaefflerf <florian.schaeffler1@hs-augsburg.de>
2022-12-05 10:24:55 +0100

    Initial commit

---
+++ drawer.js
@@ -0,0 +1,354 @@
+class Point2D {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+class AbstractShape {
+    constructor() {
+        this.id = AbstractShape.counter++;
+    }
+}
+AbstractShape.counter = 0;
+class AbstractFactory {
+    constructor(shapeManager) {
+        this.shapeManager = shapeManager;
+    }
+    handleMouseDown(x, y) {
+        this.from = new Point2D(x, y);
+    }
+    handleMouseUp(x, y) {
+        // remove the temp line, if there was one
+        if (this.tmpShape) {
+            this.shapeManager.removeShapeWithId(this.tmpShape.id, false);
+        }
+        this.shapeManager.addShape(this.createShape(this.from, new Point2D(x, y)));
+        this.from = undefined;
+    }
+    handleMouseMove(x, y) {
+        // show temp circle only, if the start point is defined;
+        if (!this.from) {
+            return;
+        }
+        if (!this.tmpTo || (this.tmpTo.x !== x || this.tmpTo.y !== y)) {
+            this.tmpTo = new Point2D(x, y);
+            if (this.tmpShape) {
+                // remove the old temp line, if there was one
+                this.shapeManager.removeShapeWithId(this.tmpShape.id, false);
+            }
+            // adds a new temp line
+            this.tmpShape = this.createShape(this.from, new Point2D(x, y));
+            this.shapeManager.addShape(this.tmpShape);
+        }
+    }
+}
+export class Line extends AbstractShape {
+    constructor(from, to) {
+        super();
+        this.from = from;
+        this.to = to;
+    }
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.moveTo(this.from.x, this.from.y);
+        ctx.lineTo(this.to.x, this.to.y);
+        ctx.stroke();
+    }
+}
+export class LineFactory extends AbstractFactory {
+    constructor(shapeManager) {
+        super(shapeManager);
+        this.label = "Linie";
+    }
+    createShape(from, to) {
+        return new Line(from, to);
+    }
+}
+class Circle extends AbstractShape {
+    constructor(center, radius) {
+        super();
+        this.center = center;
+        this.radius = radius;
+    }
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+}
+export class CircleFactory extends AbstractFactory {
+    constructor(shapeManager) {
+        super(shapeManager);
+        this.label = "Kreis";
+    }
+    createShape(from, to) {
+        return new Circle(from, CircleFactory.computeRadius(from, to.x, to.y));
+    }
+    static computeRadius(from, x, y) {
+        const xDiff = (from.x - x), yDiff = (from.y - y);
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+}
+class Rectangle extends AbstractShape {
+    constructor(from, to) {
+        super();
+        this.from = from;
+        this.to = to;
+    }
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.strokeRect(this.from.x, this.from.y, this.to.x - this.from.x, this.to.y - this.from.y);
+        ctx.stroke();
+    }
+}
+export class RectangleFactory extends AbstractFactory {
+    constructor(shapeManager) {
+        super(shapeManager);
+        this.label = "Rechteck";
+    }
+    createShape(from, to) {
+        return new Rectangle(from, to);
+    }
+}
+class Triangle extends AbstractShape {
+    constructor(p1, p2, p3) {
+        super();
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.moveTo(this.p1.x, this.p1.y);
+        ctx.lineTo(this.p2.x, this.p2.y);
+        ctx.lineTo(this.p3.x, this.p3.y);
+        ctx.lineTo(this.p1.x, this.p1.y);
+        ctx.stroke();
+    }
+}
+export class TriangleFactory {
+    constructor(shapeManager) {
+        this.shapeManager = shapeManager;
+        this.label = "Dreieck";
+    }
+    handleMouseDown(x, y) {
+        if (this.tmpShape) {
+            this.shapeManager.removeShapeWithId(this.tmpShape.id, false);
+            this.shapeManager.addShape(new Triangle(this.from, this.tmpTo, new Point2D(x, y)));
+            this.from = undefined;
+            this.tmpTo = undefined;
+            this.tmpLine = undefined;
+            this.thirdPoint = undefined;
+            this.tmpShape = undefined;
+        }
+        else {
+            this.from = new Point2D(x, y);
+        }
+    }
+    handleMouseUp(x, y) {
+        // remove the temp line, if there was one
+        if (this.tmpLine) {
+            this.shapeManager.removeShapeWithId(this.tmpLine.id, false);
+            this.tmpLine = undefined;
+            this.tmpTo = new Point2D(x, y);
+            this.thirdPoint = new Point2D(x, y);
+            this.tmpShape = new Triangle(this.from, this.tmpTo, this.thirdPoint);
+            this.shapeManager.addShape(this.tmpShape);
+        }
+    }
+    handleMouseMove(x, y) {
+        // show temp circle only, if the start point is defined;
+        if (!this.from) {
+            return;
+        }
+        if (this.tmpShape) { // second point already defined, update temp triangle
+            if (!this.thirdPoint || (this.thirdPoint.x !== x || this.thirdPoint.y !== y)) {
+                this.thirdPoint = new Point2D(x, y);
+                if (this.tmpShape) {
+                    // remove the old temp line, if there was one
+                    this.shapeManager.removeShapeWithId(this.tmpShape.id, false);
+                }
+                // adds a new temp triangle
+                this.tmpShape = new Triangle(this.from, this.tmpTo, this.thirdPoint);
+                this.shapeManager.addShape(this.tmpShape);
+            }
+        }
+        else { // no second point fixed, update tmp line
+            if (!this.tmpTo || (this.tmpTo.x !== x || this.tmpTo.y !== y)) {
+                this.tmpTo = new Point2D(x, y);
+                if (this.tmpLine) {
+                    // remove the old temp line, if there was one
+                    this.shapeManager.removeShapeWithId(this.tmpLine.id, false);
+                }
+                // adds a new temp line
+                this.tmpLine = new Line(this.from, this.tmpTo);
+                this.shapeManager.addShape(this.tmpLine);
+            }
+        }
+    }
+}
+class ToolArea {
+    constructor(shapesSelector, menue) {
+        this.selectedShape = undefined;
+        const domElms = [];
+        shapesSelector.forEach(sl => {
+            const domSelElement = document.createElement("li");
+            domSelElement.innerText = sl.label;
+            menue.appendChild(domSelElement);
+            domElms.push(domSelElement);
+            domSelElement.addEventListener("click", () => {
+                selectFactory.call(this, sl, domSelElement);
+            });
+        });
+        function selectFactory(sl, domElm) {
+            // remove class from all elements
+            for (let j = 0; j < domElms.length; j++) {
+                domElms[j].classList.remove("marked");
+            }
+            this.selectedShape = sl;
+            // add class to the one that is selected currently
+            domElm.classList.add("marked");
+        }
+    }
+    getSelectedShape() {
+        return this.selectedShape;
+    }
+}
+class Canvas {
+    constructor(canvasDomElement, toolarea) {
+        this.shapes = {};
+        const { width, height } = canvasDomElement.getBoundingClientRect();
+        this.width = width;
+        this.height = height;
+        this.ctx = canvasDomElement.getContext("2d");
+        canvasDomElement.addEventListener("mousemove", createMouseHandler("handleMouseMove"));
+        canvasDomElement.addEventListener("mousedown", createMouseHandler("handleMouseDown"));
+        canvasDomElement.addEventListener("mouseup", createMouseHandler("handleMouseUp"));
+        function createMouseHandler(methodName) {
+            return function (e) {
+                e = e || window.event;
+                if ('object' === typeof e) {
+                    const btnCode = e.button, x = e.pageX - this.offsetLeft, y = e.pageY - this.offsetTop, ss = toolarea.getSelectedShape();
+                    // if left mouse button is pressed,
+                    // and if a tool is selected, do something
+                    if (e.button === 0 && ss) {
+                        const m = ss[methodName];
+                        // This in the shapeFactory should be the factory itself.
+                        m.call(ss, x, y);
+                    }
+                }
+            };
+        }
+    }
+    draw() {
+        // TODO: it there a better way to reset the canvas?
+        this.ctx.beginPath();
+        this.ctx.fillStyle = 'lightgrey';
+        this.ctx.fillRect(0, 0, this.width, this.height);
+        this.ctx.stroke();
+        // draw shapes
+        this.ctx.fillStyle = 'black';
+        for (let id in this.shapes) {
+            this.shapes[id].draw(this.ctx);
+        }
+        return this;
+    }
+    addShape(shape, redraw = true) {
+        this.shapes[shape.id] = shape;
+        return redraw ? this.draw() : this;
+    }
+    removeShape(shape, redraw = true) {
+        const id = shape.id;
+        delete this.shapes[id];
+        return redraw ? this.draw() : this;
+    }
+    removeShapeWithId(id, redraw = true) {
+        delete this.shapes[id];
+        return redraw ? this.draw() : this;
+    }
+    apply(event) {
+        if (event instanceof ShapeAddedEvent)
+            this.addShape(event.shape, event.redraw);
+        console.log(event);
+        switch (event.type) {
+            case "shapeAdded":
+                this.addShape(event.shape, event.redraw);
+                break;
+            case "shapeRemoved":
+                this.removeShape(event.shape, event.redraw);
+                break;
+            case "shapeRemovedWithId":
+                this.removeShapeWithId(event.shapeID, event.redraw);
+        }
+    }
+}
+class ShapeEvent {
+}
+class ShapeAddedEvent extends ShapeEvent {
+    constructor(shape, redraw) {
+        super();
+        this.shape = shape;
+        this.redraw = redraw;
+        this.type = "shapeAdded";
+    }
+}
+class ShapeRemovedEvent extends ShapeEvent {
+    constructor(shape, redraw) {
+        super();
+        this.shape = shape;
+        this.redraw = redraw;
+        this.type = "shapeRemoved";
+    }
+}
+class ShapeRemovedWithIdEvent extends ShapeEvent {
+    constructor(shapeId, redraw) {
+        super();
+        this.shapeId = shapeId;
+        this.redraw = redraw;
+        this.type = "shapeRemovedWithId";
+    }
+}
+function init() {
+    const canvasDomElm = document.getElementById("drawArea");
+    const menu = document.getElementsByClassName("tools");
+    const eventStream = {
+        handlers: [],
+        registerListener(handler) {
+            this.handlers.push(handler);
+        },
+        apply(e) {
+        }
+    };
+    // Problem here: Factories needs a way to create new Shapes, so they
+    // have to call a method of the canvas.
+    // The canvas on the other side wants to call the event methods
+    // on the toolbar, because the toolbar knows what tool is currently
+    // selected.
+    // Anyway, we do not want the two to have references on each other
+    let canvas;
+    const sm = {
+        addShape(s, rd) {
+            canvas.apply(new ShapeAddedEvent(s, rd));
+            return this;
+        },
+        removeShape(s, rd) {
+            canvas.apply(new ShapeRemovedEvent(s, rd));
+            return this;
+        },
+        removeShapeWithId(id, rd) {
+            canvas.apply(new ShapeRemovedWithIdEvent(id, rd));
+            return this;
+        }
+    };
+    const shapesSelector = [
+        new LineFactory(sm),
+        new CircleFactory(sm),
+        new RectangleFactory(sm),
+        new TriangleFactory(sm)
+    ];
+    const toolArea = new ToolArea(shapesSelector, menu[0]);
+    canvas = new Canvas(canvasDomElm, toolArea);
+    canvas.draw();
+}
+init();
+//# sourceMappingURL=drawer.js.map
